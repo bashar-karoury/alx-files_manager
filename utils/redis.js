30a211ae61f9cddfcca3290dc260d25b86cc2b26
bashar_karoury <basharalkaroury@hotmail.com>
2024-12-09 14:39:54 +0300

    Updated files: utils/redis.js

---
+++ utils/redis.js
@@ -0,0 +1,40 @@
+// RedisClient should have:
+
+// the constructor that creates a client to Redis:
+// any error of the redis client must be displayed in the console (you should use on('error') of the redis client)
+// a function isAlive that returns true when the connection to Redis is a success otherwise, false
+// an asynchronous function get that takes a string key as argument and returns the Redis value stored for this key
+// an asynchronous function set that takes a string key, a value and a duration in second as arguments to store it in Redis (with an expiration set by the duration argument)
+// an asynchronous function del that takes a string key as argument and remove the value in Redis for this key
+// After the class definition, create and export an instance of RedisClient called redisClient.
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('connect', () => {});
+    this.client.on('error', (err) => {
+      console.error('Error', err);
+    });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isOpen;
+  }
+
+  async get(key) {
+    return this.client.get(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value, { EX: duration });
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
